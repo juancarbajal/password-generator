3c09eff28fc1f2dfa939cb88ef2e3a74cb2f31e4
Juan Carbajal <juancarbajal@gmail.com>
2024-07-11 15:24:33 -0500

    remove timer

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/spf13/cobra"
 
@@ -10,13 +9,6 @@ import (
 )
 
 const MAX_SIZE = 256
-
-func timer(name string) func() {
-	start := time.Now()
-	return func() {
-		fmt.Printf("%s took %v\n", name, time.Since(start))
-	}
-}
 
 var noNumbers bool
 var noSymbols bool
@@ -32,11 +24,10 @@ var rootCmd = &cobra.Command{
 
 // main ...
 func main() {
-	defer timer("main")()
 	rootCmd.Flags().BoolVarP(&noSymbols, "no-symbols", "x", false, "Do not include special characters")
 	rootCmd.Flags().BoolVarP(&noNumbers, "no-numbers", "n", false, "Do not include numbers")
 	rootCmd.Flags().IntVarP(&size, "size", "s", MAX_SIZE, "Size of the password")
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
 	}
 }
